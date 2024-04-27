9c9166abc4467e97ba33d8f14c070b2fa74148cb
Sakir Mohammad <sakirmohammad775@gmail.com>
2024-04-27 20:28:17 +0600

    secure admin pannel

--- src/Dashboard/AllUsers.jsx
+++ src/Dashboard/AllUsers.jsx
@@ -1,11 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import UseAxiosSecure from "../Hooks/UseAxiosSecure";
 
 
 const AllUsers = () => {
+    const axiosSecure=UseAxiosSecure()
+    const {data:users=[]}=useQuery({
+    queryKey:['users'],
+    queryFn:async()=>{
+        const res=await axiosSecure.get('users')
+        return res.data
+    }
+    })
     return (
         <div>
             <div className="">
                 <h3 className="text-3xl">All Users</h3>
-                <h3 className="text-3xl">Total Users</h3>
+                <h3 className="text-3xl">Total Users:{users.length}</h3>
             </div>
         </div>
     );
