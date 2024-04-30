ccd8b36c32ddab99676dbdae84378d154244facf
Sakir Mohammad <sakirmohammad775@gmail.com>
2024-04-30 23:45:49 +0600

    verification with axios

--- src/Hooks/UseAxiosSecure.jsx
+++ src/Hooks/UseAxiosSecure.jsx
@@ -4,6 +4,26 @@ export const AxiosSecure = axios.create({
     baseURL:'http://localhost:5000'
 })
 const UseAxiosSecure = () => {
+    //request interceptor to add authorization header for every secure call to the api
+    AxiosSecure.interceptors.request.use(function(config){
+        const token=localStorage.getItem('access-token')
+        console.log('request stopped by interceptors',token);
+        config.headers.authorization=`Bearer ${token}`
+        return config
+    },function(error){
+        //do something with request error
+        return Promise.reject(error)
+    })
+
+    //intercepts 401 and 403 status
+    AxiosSecure.interceptors.response.use(function(response){
+        return response
+    },(error)=>{
+        const status =error.response.status;
+        console.log('status error in the interceptor',status);
+        return Promise.reject(error)
+    })
+
     return AxiosSecure
 };
 
